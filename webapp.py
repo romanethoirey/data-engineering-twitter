e05de0ca8ca0a47d7362e3dd98e6135d85761803
gremytrullier <gremy.moi@gmail.com>
2020-12-11 01:05:58 +0100

    tweets.py just here but not used (flask good practises ?) all is in webapp

--- webapp.py
+++ webapp.py
@@ -1,13 +1,80 @@
+import re
+
+import numpy as np
+import pandas as pd
+import gensim.downloader as api
+
+from gensim.models import TfidfModel
+from gensim.corpora import Dictionary
+from gensim.similarities import SoftCosineSimilarity
+from gensim.models import WordEmbeddingSimilarityIndex
+from gensim.similarities import SparseTermSimilarityMatrix
+from gensim.utils import simple_preprocess, simple_tokenize
 
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+def preprocess(doc):
+    """
+    Tokenize, clean up input document string
+    Parameters:
+        - doc : the string to be preprocessed
+    """
+    doc = re.sub(r'pic.twitter.com\/+\w{0,}', 'picture_token', doc)
+    doc = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', " url_token ", doc)
+    return [token for token in simple_preprocess(doc, min_len=0, max_len=float("inf")) if token not in stopwords]
+
+df = pd.read_csv('tweets.csv', index_col=False)
+tweets = df['text']
+#del df
+
+print("Tweets loaded")
+
+######################
+
+print("Start preprocessing")
+
+documents = tweets
+stopwords = ['the', 'and', 'are', 'a']
+
+# Preprocess the documents, including the query string
+corpus = [preprocess(document) for document in documents]
+
+print("Preprocessing finished")
+
+###############################
+
+print("Loading model")
+# Load the model: this is a big file, can take a while to download and open
+glove = api.load("glove-wiki-gigaword-50")    
+similarity_index = WordEmbeddingSimilarityIndex(glove)
+
+print("Model loaded")
+
 def get_N_Most_Similar_Tweets(sentence, n=20):
-    tweets = []
-    for i in range(1,int(n)+1):
-        tweets.append("Top "+str(i)+" similar tweet")
-    return tweets
+    # preprocess the input sentence
+    query = preprocess(sentence)
+    
+    print("Building term dictionary and similarity matrix")
+    # Build the term dictionary, TF-idf model
+    dictionary = Dictionary(corpus+[query])
+    tfidf = TfidfModel(dictionary=dictionary)
+
+    # Create the term similarity matrix.  
+    similarity_matrix = SparseTermSimilarityMatrix(similarity_index, dictionary, tfidf)
+
+    print("Term dictionary and similarity matrix created")
+
+    query_tf = tfidf[dictionary.doc2bow(query)]
+
+    index = SoftCosineSimilarity(tfidf[[dictionary.doc2bow(document) for document in corpus]],similarity_matrix)
+
+    doc_similarity_scores = index[query_tf]
+
+    # Output the sorted similarity scores and documents
+    sorted_indexes = np.argsort(doc_similarity_scores)[::-1]
+    return sorted_indexes[:n]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
